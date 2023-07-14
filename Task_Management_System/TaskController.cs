f27ccffd436e5dec7b599a1b04a01160154ff052
SergeiySt <ser.st.1989@gmail.com>
2023-07-14 23:39:03 +0300

    add_456

---
+++ Task_Management_System/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_System
+{
+    internal class TaskController
+    {
+        private TaskRepository repository;
+
+        public TaskController(TaskRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<Task> GetTasks()
+        {
+            return repository.GetTasks();
+        }
+    }
+}
---
+++ Task_Management_System/TaskRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Management_System
+{
+    internal class TaskRepository
+    {
+        private string connectionString;
+
+        public TaskRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Task> GetTasks()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Task_table";
+                return connection.Query<Task>(query).ToList();
+            }
+        }
+    }
+}
